43dab09699678dc8dd42e4d19242080699385cc4
DRECoursera <123134721+DRECoursera@users.noreply.github.com>
2023-01-24 18:34:18 +0800

    Add files via upload

---
+++ Week 4 Test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Dane Etheridge**
+# Consultant
+
+# *I am interested in data science because I want to better manage teams of data scientists.*
+
+# ### The text in the following cell is supposed to add up to single digit integers
+
+# In[1]:
+
+
+1+1
+
+
+# <s>strikethough<s>
+# <a>abc.net.au<a>
+# <ul>
+#   <li>Coffee</li>
+#   <li>Tea</li>
+#   <li>Milk</li>
+# </ul>
+
+# In[ ]:
+
+
+
+
